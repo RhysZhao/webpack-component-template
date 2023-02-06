76f4b3bfc25c7fd4b46d7ef95ca9b68399e3b5d4
rhys.zhao <zhaorx2012@163.com>
2023-02-06 17:19:31 +0800

    Update 修改babel配置为babelrc.js文件

---
+++ .babelrc.js
@@ -0,0 +1,10 @@
+/*
+ * Author  rhys.zhao
+ * Date  2022-06-22 18:24:06
+ * LastEditors  rhys.zhao
+ * LastEditTime  2023-02-06 17:18:59
+ * Description babel配置
+ */
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react']
+};
